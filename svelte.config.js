f8ef22f71481ece5ec2d27d85a43442a13d6053d
dev4life <qingya@dtstack.com>
2021-06-19 09:50:35 +0800

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+// const config = {
+// 	kit: {
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
 
-export default config;
+const sveltePreprocess = require('svelte-preprocess');
+const pkg = require('./package.json');
+
+// export default config;
+
+module.exports = {
+	  kit: {
+          adapter: require('@sveltejs/adapter-static')(),
+	  }
+};
